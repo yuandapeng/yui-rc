131a9137a52c214b1041b9a35e6a05ecb4516dd3
yuandapeng <18852369945@163.com>
2023-01-31 22:36:43 +0800

    fix: 修复basePath

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   outputPath: 'docs-dist',
   publicPath: '/yui-rc/',
+  base: '/yui-rc/',
   themeConfig: {
     name: 'yui-rc',
   },
